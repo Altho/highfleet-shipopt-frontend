7b29bf13aa7a3f5c3d79b45175dd00030c7b0db1
Altho <alan.at.thomas@gmail.com>
2022-09-01 11:55:22 +0200

    created list components to render modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
+import { Select } from '@mantine/core';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import SelectModule from '../components/Select-Module/SelectModule';
 
-export default function HomePage() {
-  return (
-    <>
-      <ColorSchemeToggle />
-    </>
-  );
+export const getServerSideProps = async () => {
+  const req = await fetch('https://eo2qdk3mdwiezc2mj46p2oilxq0gfpwr.lambda-url.us-east-1.on.aws/');
+  const res = await req.json();
+
+  const { modules, constraints } = res;
+
+  console.log('moduuuules', modules);
+
+  return {
+    props: {
+      modules,
+      constraints,
+    },
+  };
+};
+
+export default function HomePage({ modules, constraints }) {
+return (
+  <>
+    <SelectModule modules={modules} />
+    <SelectModule modules={constraints} />
+    <ColorSchemeToggle />
+  </>
+);
 }
